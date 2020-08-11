14852dbfe8e16193367c51ca42a99e9138eaaf08
kakoi-to-pirat <kakoi-to.pirat@yandex.ru>
2020-08-11 14:52:57 +0300

    add database production-enviroment

--- src/client_card/db.clj
+++ src/client_card/db.clj
@@ -1,18 +1,37 @@
 (ns client-card.db
-  (:require [clojure.java.jdbc :as sql]))
+  (:require [clojure.java.jdbc :as sql]
+            [dotenv :refer [env]]
+            [clojure.string :as string]))
 
 
 ;; -------------------------
 ;; CONFIG
 
 
-(def db-spec {:dbtype "postgresql"
-              :name "postgres"
+(def db-spec-dev {:dbtype (env "DBTYPE")
+                  :name (env "NAME")
+                  :subprotocol (env "SUBPROTOCOL")
+                  :host (env "HOST")
+                  :subname (env "SUBNAME")
+                  :user (env "USER")
+                  :password (env "USER")})
+
+(def db-uri
+  (java.net.URI. (or (System/getenv "DATABASE_URL") "")))
+
+(def user-and-password
+  (if (nil? (.getUserInfo db-uri))
+    nil (string/split (.getUserInfo db-uri) #":")))
+
+(def db-spec {:classname "org.postgresql.Driver"
               :subprotocol "postgresql"
-              :host "localhost"
-              :subname "//localhost:5432/postgres"
-              :user "postgres"
-              :password "postgres"})
+              :user (get user-and-password 0)
+              :password (get user-and-password 1)
+              :subname (if (= -1 (.getPort db-uri))
+                         (format "//%s%s" (.getHost db-uri) (.getPath db-uri))
+                         (format "//%s:%s%s" (.getHost db-uri) (.getPort db-uri) (.getPath db-uri)))})
+
+(defn get-db-spec [] (if (env "DEV") db-spec-dev db-spec))
 
 
 ;; -------------------------
@@ -33,11 +52,11 @@
 
 
 (defn create-cards-table []
-  (sql/db-do-commands db-spec cards-table-ddl)
+  (sql/db-do-commands (get-db-spec) cards-table-ddl)
   (println "Client cards table was created is successful"))
 
 (defn drop-cards-table []
-  (sql/db-do-commands db-spec [(sql/drop-table-ddl :cards)])
+  (sql/db-do-commands (get-db-spec) [(sql/drop-table-ddl :cards)])
   (println "Client cards table was removed is successful"))
 
 
@@ -46,7 +65,7 @@
 
 
 (defn db-schema-migrated? [table-name]
-  (-> (sql/query db-spec
+  (-> (sql/query (get-db-spec)
                  ["SELECT count (*) 
                    FROM information_schema.tables
                    WHERE table_name = ?" table-name])
@@ -68,24 +87,24 @@
 
 
 (defn get-all-cards []
-  (sql/query db-spec ["SELECT * FROM cards"]))
+  (sql/query (get-db-spec) ["SELECT * FROM cards"]))
 
 (defn get-card [id]
-  (sql/query db-spec ["SELECT * FROM cards WHERE id = ?" id]))
+  (sql/query (get-db-spec) ["SELECT * FROM cards WHERE id = ?" id]))
 
 (defn create-card [data]
   (let [{:keys [full_name, gender, address, birthday, id_policy]} data]
-    (sql/execute! db-spec ["INSERT INTO 
+    (sql/execute! (get-db-spec) ["INSERT INTO 
                             cards (full_name, gender, address, birthday, id_policy) 
                             VALUES (?, ?, ?, ?::date, ?::bigint)"
-                           full_name gender address birthday id_policy])))
+                                 full_name gender address birthday id_policy])))
 
 (defn update-card [id data]
   (let [{:keys [full_name, gender, address, birthday, id_policy]} data]
-    (sql/execute! db-spec ["UPDATE public.cards 
+    (sql/execute! (get-db-spec) ["UPDATE public.cards 
                             SET full_name = ?, gender = ?, address = ?, birthday = ?::date, id_policy = ?::bigint
                             WHERE id = ?::integer"
-                           full_name gender address birthday id_policy id])))
+                                 full_name gender address birthday id_policy id])))
 
 (defn delete-card [id]
-  (sql/execute! db-spec ["DELETE FROM cards WHERE id = ?" id]))
+  (sql/execute! (get-db-spec) ["DELETE FROM cards WHERE id = ?" id]))
